132348355e0bdbfb83a49f83983bfae702e8d053
SU <suyiju@gmail.com>
2024-05-21 22:57:33 +0800

    Initial Commit

---
+++ add figure/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  add figure
+//
+//  Created by SU YI-JU on 2024/5/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
